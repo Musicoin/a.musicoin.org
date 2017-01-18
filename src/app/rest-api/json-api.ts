70ad6b85754585ac6930ae7a6a2f9900800c0bcc
Dan Phifer <dan.phifer@gmail.com>
2017-01-18 12:41:57 +0800

    * Removing placeholder text when the artist name is not found (happens for pre-v0.7 contracts)

--- src/app/rest-api/json-api.ts
+++ src/app/rest-api/json-api.ts
@@ -105,7 +105,7 @@ export class MusicoinOrgJsonAPI {
               license.artistName = record.artistName;
             }
             else {
-              license.artistName = "Not found";
+              license.artistName = "";
             }
             return license;
           })
