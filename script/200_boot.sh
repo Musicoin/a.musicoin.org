bbd2180bcb29f1f2aadcc626bd159f20b3b9b161
Dan Phifer <dan.phifer@gmail.com>
2017-01-31 17:57:42 +0800

    * Testing jenkins

--- script/200_boot.sh
+++ script/200_boot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-echo "Starting ipfs..."
+echo "Starting ipfs... "
 cd /
 /go-ipfs/ipfs daemon --init=true --migrate=true >> ipfs.log 2>&1 &
 
@@ -9,4 +9,5 @@ forever start /musicoin.org/src/server.js
 
 echo "Setting up jenkins monitor.."
 touch /home/jenkins/monitor
+chown jenkins /home/jenkins/monitor
 /musicoin.org/script/watch-file.sh /home/jenkins/monitor /musicoin.org/script/302_update.sh >> mc_watch.log 2>&1 &
