3b4a3732a910502604e124a5c37c5e0cb0a9bea0
Dan Phifer <dan.phifer@gmail.com>
2017-01-31 10:57:46 +0800

    * fixes in startup script

--- script/200_boot.sh
+++ script/200_boot.sh
@@ -9,4 +9,4 @@ forever start /musicoin.org/src/server.js
 
 echo "Setting up jenkins monitor.."
 touch /home/jenkins/monitor
-/musicoin.org/script/watch-file.sh /home/jenkins/monitor /musicoin.org/script/update.sh >> mc_watch.log 2>&1 &
+/musicoin.org/script/watch-file.sh /home/jenkins/monitor /musicoin.org/script/302_update.sh >> mc_watch.log 2>&1 &
--- script/watch-file.sh
+++ script/watch-file.sh
@@ -3,7 +3,7 @@
 while [ true ]
 do
     echo "Waiting for changes to $1..."
-    if inotifywait -e modify $1
+    if inotifywait $1
     then
       echo "Detected changes, running $2"
       eval $2
