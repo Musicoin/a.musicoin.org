d193f94bda4a4c92c82405b0675e2f7a5899cfc3
Dan Phifer <dan.phifer@gmail.com>
2017-03-03 16:43:28 +0800

    * removing redirect temporarily

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -81,9 +81,10 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
     return new Date(timestamp * 1000).toLocaleDateString('en-US', options);
   }
 
-  app.get('/', unauthRedirect("/info"), checkLoginRedirect, function (req, res) {
-    res.render('index-frames.ejs', {mainFrameLocation: "/main"});
-  });
+  app.get('/', (req, res) => doRender(req, res, 'info.ejs', {}));
+  // app.get('/', unauthRedirect("/info"), checkLoginRedirect, function (req, res) {
+  //   res.render('index-frames.ejs', {mainFrameLocation: "/main"});
+  // });
 
   app.get('/accept/:code', (req, res) => {
     console.log("host: " + req.get('host'));
