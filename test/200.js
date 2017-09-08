5b03e23d8cdb5753179c6273d300179875d1715c
Varunram <vrg2009@ymail.com>
2017-09-08 18:54:46 +0530

    Add a couple of more tests

--- test/200.js
+++ test/200.js
@@ -1,20 +1,46 @@
 var chai = require('chai');
 var chaiHttp = require('chai-http');
 var app = require('../src/server.js');
-
 var expect = chai.expect;
-
 chai.use(chaiHttp);
 
-describe('Running Fine', function() {
-  describe('/', function() {
-    it('responds with status 200', function(done) {
-      chai.request(app)
-        .get('/')
-        .end(function(err, res) {
-          expect(res).to.have.status(200);
-          done();
-        });
-    });
+describe('> Main App', function() {
+  it('> Main App', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+describe('> Sign Up Page', function() {
+  it('> Sign Up Page', function(done) {
+    chai.request(app)
+      .get('/welcome')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+describe('> How it Works', function() {
+  it('How It Works Page is Up, but this is a static site', function(done) {
+    chai.request(app)
+      .get('/howitworks.html')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+describe('> Password Reset Page', function() {
+  it('Password Reset page is up', function(done) {
+    chai.request(app)
+      .get('/login/forgot')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
   });
 });
