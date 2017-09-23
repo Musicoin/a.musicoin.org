a72b4cbc5aa48449f35f0e924e81ac49321fd4ed
Varunram <vrg2009@ymail.com>
2017-09-23 22:41:41 +0530

    Try to fix nav/nav/feed

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -314,12 +314,6 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
     });
   });
 
-  // anything under "/nav/" is a pseudo url that indicates the location of the mainFrame
-  // e.g. /nav/xyz will be re-routed to "/" with a parameter that sets the mainFrame url to "xyz"
-  app.get('/nav/*', isLoggedInOrIsPublic, (req, res) => {
-    res.render('index-frames.ejs', { mainFrameLocation: req.originalUrl.substr(4) });
-  });
-
   // =====================================
   // HOME PAGE
   // =====================================
