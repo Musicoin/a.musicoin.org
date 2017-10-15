b6f43418a218b151165e991f69465f534cff3abc
Varunram <vrg2009@ymail.com>
2017-10-15 16:14:43 +0530

    Fix Playback time

--- src/config/config.js
+++ src/config/config.js
@@ -29,7 +29,7 @@ function getStructuredConfig(keyValueConfig) {
         },
         ipSessionChangeTimeout: 1000 * 60 * 10,
         playbackLinkTTLMillis: 1000 * 60 * 30,
-        freePlayDelay: 1000 * 600,
+        freePlayDelay: 1000 * 60,
         musicoinAdminProfile: keyValueConfig.musicoinAdminProfile,
         termsOfUseVersion: keyValueConfig.termsOfUseVersion,
         database: {
