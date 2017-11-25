fe68a585bdaf90515cee874194545bb28452eda4
Varunram <vrg2009@ymail.com>
2017-11-25 15:35:49 +0530

    Resolve mocha 4.0 problem

--- test/200.js
+++ test/200.js
@@ -3,32 +3,29 @@ var chaiHttp = require('chai-http');
 var app = require('../src/server.js');
 var expect = chai.expect;
 chai.use(chaiHttp);
+const request = chai.request(app);
 
-  it('Main App', function() {
-    chai.request(app)
-      .get('/')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-      });
-  });
-  it('Sign Up Page', function() {
-    chai.request(app)
-      .get('/welcome')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-      });
-  });
-  it('How It Works Page is Up, but this is a static site', function() {
-    chai.request(app)
-      .get('/howitworks.html')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-      });
-  });
-  it('Password Reset page is up', function() {
-    chai.request(app)
-      .get('/login/forgot')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-      });
-  });
+it('Main App', function() {
+  request.get('/')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+    });
+});
+it('Sign Up Page', function() {
+  request.get('/welcome')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+    });
+});
+it('How It Works Page is Up, but this is a static site', function() {
+  request.get('/howitworks.html')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+    });
+});
+it('Password Reset page is up', function() {
+  request.get('/login/forgot')
+    .end(function(err, res) {
+      expect(res).to.have.status(200);
+    });
+});
