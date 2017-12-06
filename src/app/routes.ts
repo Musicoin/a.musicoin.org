a1ec4110366a8d384e0aa9b050f9b228bc3b6540
Tamil Vendhan <tamil@mymandir.com>
2017-12-06 11:32:08 +0800

    Sending license or artist data to render function call for rendering metatags

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -367,25 +367,28 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
 
   // anything under "/nav/" is a pseudo url that indicates the location of the mainFrame
   // e.g. /nav/xyz will be re-routed to "/" with a parameter that sets the mainFrame url to "xyz"
-  app.get('/nav/*', isLoggedInOrIsPublic, (req, res) => {
-    console.log(req.path, '/nav/*');
-    res.render('index-frames.ejs', { mainFrameLocation: req.originalUrl.substr(4) });
-  });
-
   // app.get('/nav/*', isLoggedInOrIsPublic, (req, res) => {
-  //   let mainFrameLocation = req.originalUrl.substr(4);
-  //   let promise = null;
-  //   if(req.path.indexOf('track') !== -1) {
-  //     promise = jsonAPI.getLicense(req.params.address)
-  //       .then(license => license ? Promise.resolve({ mainFrameLocation: mainFrameLocation, license: license}) : Promise.reject());
-  //   }
-  //   else {
-  //     promise = jsonAPI.getArtist(req.params.address, false, false).then((result) => Promise.resolve(...result, mainFrameLocation: mainFrameLocation}));
-  //   }
-  //   promise
-  //   .then((result) => res.render('index-frames.ejs', result), () => res.render('not-found.ejs'))
-  //   .catch(() => res.render('not-found.ejs'));
+  //   console.log(req.path, '/nav/*');
+  //   res.render('index-frames.ejs', { mainFrameLocation: req.originalUrl.substr(4) });
   // });
+
+  app.get('/nav/*', isLoggedInOrIsPublic, (req, res) => {
+    
+    let mainFrameLocation = req.originalUrl.substr(4);
+    let promise = null;
+    if(req.path.indexOf('track') !== -1) {
+      promise = jsonAPI.getLicense(req.params.address)
+        .then(license => license ? Promise.resolve({ mainFrameLocation: mainFrameLocation, license: license}) : Promise.reject());
+    }
+    else {
+      promise = jsonAPI.getArtist(req.params.address, false, false).then((result) => Promise.resolve(...result, mainFrameLocation: mainFrameLocation}));
+    }
+
+    promise
+    .then((result) => res.render('index-frames.ejs', result), () => res.render('not-found.ejs'))
+    .catch(() => res.render('not-found.ejs'));
+
+  });
 
   // =====================================
   // HOME PAGE
