e1f37cbe0f55f8419a1f6051a247e87e70b5a4ac
Varunram <vrg2009@ymail.com>
2017-12-22 22:02:50 +0530

    Remove log statements suggested

--- src/app/rest-api/json-api.ts
+++ src/app/rest-api/json-api.ts
@@ -1728,8 +1728,6 @@ export class MusicoinOrgJsonAPI {
     return this.mcHelper.getLicense(record.contractAddress)
       .bind(this)
       .then(function(license) {
-        console.log(record);
-        console.log(license);
         if (!license.artistName)
           license.artistName = record.artistName || 'Musicoin';
 
