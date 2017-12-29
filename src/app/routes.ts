2f5a44790ef48e9a6aecbea7ac2f9ebfafa94c92
Im <isaac.mao@gmail.com>
2017-12-29 19:03:05 +0800

    Revert "Fixed tumblr share url"

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1484,8 +1484,6 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
         formattedTotalUSD: "$" + _formatNumber((totalPlays + totalTips) * exchangeRate.usd, 2)
       };
       output.exchangeRate = exchangeRate;
-      output.isAuthenticated = req.isAuthenticated();
-      output.user = req.user || {};
       res.render("artist.ejs", output);
     })
   });
@@ -1522,7 +1520,6 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
         const tips = release.directTipCount || 0;
         const usd = exchangeRate.success ? "$" + _formatNumber((plays + tips) * exchangeRate.usd, 2) : "";
         res.render("track.ejs", {
-          isAuthenticated: req.isAuthenticated(),
           artist: response.artist,
           license: license,
           contributors: resolvedAddresses,
