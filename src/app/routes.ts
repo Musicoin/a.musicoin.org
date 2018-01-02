343f8a5ec15abe490194dceaa7694204d8c7e095
Varunram <vrg2009@ymail.com>
2018-01-02 17:40:44 +0530

    Edit Forum link on Header, fix length of filter

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -1349,7 +1349,7 @@ export function configure(app, passport, musicoinApi: MusicoinAPI, mediaProvider
   });
 
   app.get('/peerverif/a7565fbd8b81b42031fd893db7645856f9d6f377a188e95423702e804c7b64b1', (req, res) => {
-    const length = 2000;
+    const length = 1000;
     const start = typeof req.query.start != "undefined" ? parseInt(req.query.start) : 0;
     const previous = Math.max(0, start - length);
     const url = '/admin/users?search=' + (req.query.search ? req.query.search : '');
